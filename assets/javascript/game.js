73ba30758a446b87e51b22a8a3d952f2ad3476c8
Sameea Mughnee <samiyah.mughnee@gmail.com>
2018-01-10 22:59:38 -0500

    pseudo

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//set up the HTML & CSS Elements with corresponding ids and divs
+//Create buttons for crystals
+//Create a scorecard
+//Assing random values to the crystal buttons
+//Randomly generate a number when new game starts
+//Tally up the score when the crystal number value either equals or exceeds the randomly generated number
+//Start a game over when the crystal number values either equals or exceeds the randomly generated number
+//Tally up the total score
